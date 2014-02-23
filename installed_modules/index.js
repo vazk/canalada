80ce18ac21bae14e916341594a5d1459e01ebaf0
Vazgen Karapetyan <vazkus@gmail.com>
2014-02-23 12:06:01 -0800

    Redesigned the interface of module registrator

--- installed_modules/index.js
+++ installed_modules/index.js
@@ -7,26 +7,31 @@ InstalledModules = {
             loadLibraries(this.library);
         }
     },
-    register: function(module, library) {
+    register: function(modName, sublibName) {
         var context = this;
+        var sublib = context.library[sublibName];
+        if(sublib === undefined) {
+            sublib = [];
+            context.library[sublibName] = sublib;
+        }
         context.numModules++;
         $.ajax({
-            url: '../installed_modules/' + module + '/Properties.js', 
+            url: '../installed_modules/' + modName + '/Properties.js', 
             success: function(data){
                 var props = eval(data);
-                props.icon = '../installed_modules/' + module + '/' + props.icon;
-                props.module = module;
-                library.push(props);
+                props.icon = '../installed_modules/' + modName + '/' + props.icon;
+                props.module = modName;
+                sublib.push(props);
                 context.numCallBacks++;
                 context.checkLoadComplete();
             },
             dataType: "text"
         });
         $.ajax({
-            url: '../installed_modules/' + module + '/IO.js', 
+            url: '../installed_modules/' + modName + '/IO.js', 
             success: function(data){
                 var io = eval(data);
-                FE.registerModuleClass(module, io);
+                FE.registerModuleClass(modName, io);
                 context.numCallBacks++;
                 context.checkLoadComplete();
             },
@@ -36,34 +41,8 @@ InstalledModules = {
 
 };
 
-InstalledModules.library = { 
-        comm: [],
-        database: [],
-        other: []
-    };
 
-InstalledModules.register('EmailClient', InstalledModules.library.comm);
-InstalledModules.register('FileReader', InstalledModules.library.database);
-InstalledModules.register('FileWriter', InstalledModules.library.database);
-
-
-/*        [
-         {'src':'batch/32x32/in.png',
-          'info':{'author':'vazkus','description':'globe bla.','module':'FileWriter'}},
-         {'src':'batch/32x32/out.png',
-          'info':{'author':'vazkus','description':'globe bla.','module':'FileReader'}},
-         InstalledModules.register('EmailClient'),
-         /*{'src':'batch/32x32/@.png',
-          'info':{'author':'vazkus','description':'globe bla.','module':'EmailClient'}},
-         /*{'src':'batch/32x32/cloud-upload.png',
-          'info':{'author':'vazkus','description':'globe bla.','module':'DropboxWriter'}},
-         {'src':'batch/32x32/cloud-download.png',
-          'info':{'author':'vazkus','description':'globe bla.','module':'DropboxReader'}},
-         {'src':'batch/32x32/marquee-download.png',
-          'info':{'author':'vazkus','description':'globe bla.','module':'YoutubeDownloader'}},
-         {'src':'batch/32x32/repeat-2.png',
-          'info':{'author':'vazkus','description':'globe bla.','module':'MediaConverter'}}
-        ]
-    };
-*/
+InstalledModules.register('EmailClient', 'comm');
+InstalledModules.register('FileReader', 'database');
+InstalledModules.register('FileWriter', 'database');
 
